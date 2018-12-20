e0fe66f30255092042393c5b94f4b5d8a6286fa2
IPostevoi <baklandor410@yandex.ru>
2018-12-21 00:59:25 +0300

    initia lcommit

---
+++ app.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+import dash
+import radial
+from dash.dependencies import Input, Output, State
+import dash_core_components as dcc
+import dash_html_components as html
+import numpy as np
+import base64
+import matplotlib.pyplot as plt
+
+app = dash.Dash()
+
+app.layout = html.Div(children=[
+    html.H1(children=u'Draw your radial tree'),
+    html.P(children='Insert your tree (make sure of the indexes uniqueness)'),
+    # dcc.Input(id='input', type='text'),
+    dcc.Textarea(
+        id='input',
+        placeholder='Insert your tree here',
+        value='0,1\n0,2\n0,3\n1,4\n1,5\n4,6',
+        style={'width': '100%'}
+    ),
+    html.Button('Go', id='button_id'),
+    html.P(id='output'),
+
+    # dcc.Graph(
+    #     id='example-graph',
+    #     figure={
+    #         'data': [
+    #             {'x': x[3:], 'y': y[3:], 'type': 'line', 'name': 'KEK', 'mode': 'lines'},
+    #             {'x': x, 'y': y, 'type': 'line', 'name': 'KEK', 'mode': 'lines'},
+    #         ],
+    #         'layout': {
+    #             'title': 'KEK'
+    #             # 'height': 600,
+    #             # 'width': 600
+    #         }
+    #     }
+    # )
+], style={'width': '50%', 'display': 'inline-block', 'vertical-align': 'right'})
+
+
+def plot_tree(G, arr):
+    edges = dict()
+    for i in arr:
+        edges[i[0]] = []
+    for i in arr:
+        for j in G:
+            if i[0] == j[3] or i[1] == j[3]:
+                edges[i[0]].append((j[0], j[1]))
+
+    phi = np.linspace(0, 2*np.pi, 1000)
+    plt.clf()
+    plt.figure(figsize=(8, 8))
+
+    for key, edge in edges.items():
+        x = [i[0] for i in edge]
+        y = [i[1] for i in edge]
+        for i in range(0, len(edge)):
+            plt.plot(x[i: i+2], y[i: i+2], c='black', linestyle='-', marker='o', linewidth=0.5, markersize=3)
+    eps = 0.05
+    for i in G:
+        plt.annotate(i[3], (i[0]+eps, i[1]+eps))
+
+    for i in range(len(edges.items())+1):
+        plt.plot(i*np.cos(phi), i*np.sin(phi), c='grey', linewidth=0.3)
+    plt.axis('off')
+    plt.savefig("fig")
+
+
+
+@app.callback(
+    Output(component_id='output', component_property='children'),
+    [Input(component_id='button_id', component_property='n_clicks')],
+    state=[State(component_id='input', component_property='value')]
+)
+def on_button(n_clicks, input_data):
+    if not input_data is "":
+        a = 0
+        b = 2 * np.pi
+        root, arr = radial.create_tree(input_data)
+        G = radial.get_radial_tree(root, a, b)
+        plot_tree(G, arr)
+        encoded_image = base64.b64encode(open("fig.png", 'rb').read())
+        return html.Img(id='img', src='data:image/png;base64,{}'.format(encoded_image.decode()))
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ radial.py
@@ -0,0 +1,108 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+class Node():
+    depth = 0
+    leaves = 0
+    parent = None
+
+    def __init__(self, index, data):
+        self.index = index
+        self.children = []
+        self.data = data
+
+    def add_child(self, obj):
+        old_len = len(self.children)
+        self.children.append(obj)
+        obj.parent = self
+        # update depth and leaves
+        self.update_depth(obj)
+        self.update_leaves(obj, old_len)
+
+    def update_leaves(self, node, old_len):
+        new_len = len(self.children)
+        if old_len == 0:
+            node.leaves = 1
+            node.parent.leaves = 1
+
+        elif not old_len == new_len:
+            while True:
+                node.leaves += 1
+                if node.parent is None:
+                    break
+                node = node.parent
+
+    def update_depth(self, node):
+        node.depth = self.depth + 1
+        while not len(node.children) == 0:
+            for c in node.children:
+                self.update_depth(c)
+
+
+
+def radial_position(T, v, a, b, G):
+    D = v.depth
+    if D == 0:
+        G.append((0, 0, v.index, v.data))
+    theta = a
+    R_D = R_0 + zeta * D
+    k = v.leaves
+    for c in v.children:
+        l = c.leaves
+        mu = theta + (l / float(k) * (b - a))
+        G.append((R_D * np.cos((theta + mu) / 2), R_D * np.sin((theta + mu) / 2), c.index, c.data))
+        if len(c.children) > 0:
+            radial_position(T, c, theta, mu, G)
+
+        theta = mu
+    return G
+
+
+R_0 = 1
+zeta = 1
+def get_radial_tree(root, a, b):
+# root = Node(0)
+# p = Node(1)
+# q = Node(2)
+# r = Node(3)
+# qq = Node(4)
+# zz = Node(5)
+# zzz = Node(6)
+# zzzz = Node(7)
+#
+# p.add_child(qq)
+# p.add_child(qq)
+# p.add_child(qq)
+# qq.add_child(zzzz)
+# qq.add_child(zzz)
+# zzzz.add_child(q)
+#     a = 0
+#     b = 2 * np.p
+    G = []
+    radial_position(None, root, a, b, G)
+    return G
+
+
+def create_tree(input):
+    arr = []
+    inp = input.split('\n')
+    for i in inp:
+        j = i.split(',')
+        arr.append((j[0], j[1]))
+
+    arr1 = [i[0] for i in arr]
+    arr2 = [i[1] for i in arr]
+    st = list(dict.fromkeys(arr1+arr2))
+    dct = dict()
+    for i in range(len(st)):
+        dct[st[i]] = i
+    nodes = [Node(i, st[i]) for i in range(len(st))]
+    root = nodes[0]
+    curr = root
+    for index in range(0, len(st)-1):
+        for i in arr:
+            if i[0] == curr.data:
+                curr.add_child(nodes[dct[i[1]]])
+        curr = nodes[index + 1]
+    return root, arr
